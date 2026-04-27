95981d3732a598af26b37e3c6d04117fb589309d
seaikh91-Asif <seaikh91@gmail.com>
2026-04-27 13:21:08 +0530

    review section finish

--- responsive-delivery-website-main/assets/js/main.js
+++ responsive-delivery-website-main/assets/js/main.js
@@ -34,6 +34,21 @@ window.addEventListener('scroll', shadowHeader)
 
 /*=============== SWIPER REVIEWS ===============*/
 
+const swiperReviews = new Swiper('.reviews__swiper', {
+  loop: true,
+  spaceBetween: 16,
+  grabcursor: true,
+  speed: 600,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay:{
+    delay: 2000,
+    disableOnInteraction: false,
+  }
+})
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
