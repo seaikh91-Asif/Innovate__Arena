a9b12634a6f53e7971d72c264be590b94ceaa5d3
seaikh91-Asif <seaikh91@gmail.com>
2026-04-27 12:22:43 +0530

    finish navigation bar

--- responsive-delivery-website-main/assets/js/main.js
+++ responsive-delivery-website-main/assets/js/main.js
@@ -1,9 +1,28 @@
 /*=============== SHOW MENU ===============*/
+const showMenu = (navId, toggleId) => {
+    const nav = document.getElementById(navId),
+            toggle = document.getElementById(toggleId)
 
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu')
+    })
+
+}
+
+showMenu('nav-menu', 'nav-toggle')
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== ADD SHADOW HEADER ===============*/
 
 
